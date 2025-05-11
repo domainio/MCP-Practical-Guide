4c0edaf5fcb36139c430bdee6a3843b9dabc07e8
domainio <innovaterz@gmail.com>
2025-05-11 14:41:22 +0300

    prompts

--- src/5-mcp-prompt/mcp_client_agent.py
+++ src/5-mcp-prompt/mcp_client_agent.py
@@ -4,6 +4,7 @@ from mcp.client.sse import sse_client
 from mcp import ClientSession
 from langgraph.prebuilt import create_react_agent
 from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_mcp_adapters.prompts import load_mcp_prompt
 
 load_dotenv()
 
@@ -12,25 +13,24 @@ async def main():
         async with ClientSession(reader, writer) as session:
             await session.initialize()
             tools = await load_mcp_tools(session)
-        
+            # mcp_prompt = await load_mcp_prompt(session,"workflow")
+            # print(f"mcp_prompt: {mcp_prompt}")
+            
             agent = create_react_agent(
                 model="gpt-4.1",
                 tools=tools,
-                prompt="You are a helpful assistant"
+                # prompt=mcp_prompt[0].content
             )
-            
-            messages = []
             
             while True:
                 user_input = input("\nYou: ")
                 # Exit condition
-                messages.append({"role": "user", "content": user_input})
                 if user_input.lower() in ["exit", "quit", "bye"]:
                     print("\nAssistant: Goodbye!")
-                    break
-                response = await agent.ainvoke({"messages": messages})
+                    break        
+                
+                response = await agent.ainvoke({"messages": user_input})
                 ai_message = response["messages"][-1].content
-                messages.append(response["messages"][-1])
                 print(f"\nAssistant: {ai_message}")
                 
 if __name__ == "__main__":
