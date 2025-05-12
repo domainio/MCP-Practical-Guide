381e7b2859f142a1ab09016f7199012fb20c64d4
domainio <innovaterz@gmail.com>
2025-05-12 10:10:02 +0300

    stream

--- src/5-mcp-prompt/mcp_client_agent.py
+++ src/5-mcp-prompt/mcp_client_agent.py
@@ -5,7 +5,7 @@ from mcp import ClientSession
 from langgraph.prebuilt import create_react_agent
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_mcp_adapters.prompts import load_mcp_prompt
-
+import time
 load_dotenv()
 
 async def main():
@@ -28,12 +28,17 @@ async def main():
                 if user_input.lower() in ["exit", "quit", "bye"]:
                     print("\nAssistant: Goodbye!")
                     break        
+                print("\nAssistant: ",end="", flush=True)
+                async for chunk in agent.astream({"messages": user_input}, stream_mode="messages"):
+                    print(chunk[0].content,end="", flush=True)
+                    time.sleep(0.05)
+                print("\n")
                 
-                async for chunk in agent.astream({"messages": user_input}, stream_mode="updates"):
-                    if chunk.get("agent"):
-                        ai_message = chunk["agent"]["messages"][-1].content
-                        if ai_message:
-                            print(f"\nAssistant: {ai_message}")
+                # async for chunk in agent.astream({"messages": user_input}, stream_mode="updates"):
+                    # if chunk.get("agent"):
+                    #     ai_message = chunk["agent"]["messages"][-1].content
+                    #     if ai_message:
+                    #         print(ai_message)
                     
                 
 if __name__ == "__main__":
