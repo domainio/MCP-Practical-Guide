b5124d616558149844f03e8c94b99e601c7cc331
domainio <innovaterz@gmail.com>
2025-05-21 17:08:56 +0300

    mcp challenges sticky session

---
+++ src/7-mcp-challenges/sticky-session/backend/app/server.py
@@ -0,0 +1,34 @@
+from fastmcp import FastMCP
+import mcp.types as Resource
+import yfinance as yf
+import requests
+
+mcp = FastMCP("WeatherStockMCP")
+
+@mcp.tool(
+    name="get_weather",
+    description="Get the current weather for a given latitude and longitude.",
+)
+def get_weather(latitude: float, longitude: float) -> str:
+    print(f"latitude: {latitude}, longitude: {longitude}")
+    response = requests.get(f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m")
+    data = response.json()
+    temperature = data['current']['temperature_2m']
+    print(f"temperature: {temperature}")
+    return f"The current temperature is {temperature}°C."
+
+@mcp.tool(
+    name="get_stock_price",
+    description="Get the current stock price of a given symbol.",
+)
+def get_stock_price(symbol: str) -> str:
+    print(f"symbol: {symbol}")
+    stock_data = yf.Ticker(symbol)
+    last_price = stock_data.fast_info.last_price
+    print(f"last_price: {last_price}")
+    return f"The current price of {symbol} is ${last_price}."
+
+
+if __name__ == "__main__":
+    # mcp.run(transport="sse")
+    mcp.run(transport="sse", host="0.0.0.0", port=8000)
---
+++ src/7-mcp-challenges/sticky-session/client/mcp_client.py
@@ -0,0 +1,23 @@
+import asyncio
+from fastmcp.client.transports import SSETransport
+from fastmcp import Client
+
+
+async def main():
+    # Connect to the MCP server over SSE
+  async with Client(SSETransport("http://localhost:8080/sse")) as client:
+
+    # List available tools
+    tools = await client.list_tools()
+    print(f"\tools: {tools}")
+
+    # Call get_weather tool
+    weather_result = await client.call_tool("get_weather", {"latitude": 32.0853, "longitude": 34.7818})
+    print("\nWeather:", weather_result)
+
+    # Call get_stock_price tool
+    stock_result = await client.call_tool("get_stock_price", {"symbol": "AAPL"})
+    print("\nStock Price:", stock_result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
